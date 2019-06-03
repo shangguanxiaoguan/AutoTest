b7f20ad6f3275ec42e824f5df0b7165fda0a5d63
shangguanxiaoguan <1414122327@qq.com>
2019-06-03 17:40:25 +0800

    第11章：Mybatis

--- Chapter10/src/main/java/com/course/server/MyPostMethod.java
+++ Chapter10/src/main/java/com/course/server/MyPostMethod.java
@@ -1,10 +1,12 @@
 package com.course.server;
 
+import com.course.bean.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
@@ -31,6 +33,28 @@ public class MyPostMethod {
             return "恭喜你登录成功了！";
         }
         return "用户名或密码错误";
+    }
+
+    @RequestMapping(value = "/getUserList",method = RequestMethod.POST)
+    @ApiOperation(value = "获取用户列表",httpMethod = "POST")
+    public String getUserList(HttpServletRequest request,
+                            @RequestBody User u){  //@RequestBody 要求传入的参数是json格式的
+        User user;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies){
+            if (cookie.getName().equals("login")
+                    && cookie.getValue().equals("true")
+                    && u.getUserName().equals("zhangsan")
+                    && u.getPassWord().equals("123456")
+            ){
+                user = new User();
+                user.setName("lisi");
+                user.setAge("18");
+                user.setSex("man");
+                return user.toString();
+            }
+        }
+        return "参数不合法";
     }
 
 }
