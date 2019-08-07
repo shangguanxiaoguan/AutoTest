347eb941845bf7441dbf0c67215d502ae3dda172
shangguanxiaoguan <1414122327@qq.com>
2019-08-07 19:14:52 +0800

    使用Excel传递参数

--- hejiameng/src/main/java/com/sangame/hjm/manager/LoginByExcel.java
+++ hejiameng/src/main/java/com/sangame/hjm/manager/LoginByExcel.java
@@ -1,9 +1,16 @@
 package com.sangame.hjm.manager;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sangame.hjm.utils.ReadExcel;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class LoginByExcel {
     @DataProvider(name = "post")
@@ -12,5 +19,32 @@ public class LoginByExcel {
        // return ReadExcel.readExData(testCaseExcel,1);
         return null;
 }
+
+    @Test(dataProvider = "post")
+    //此处传递的参数必须和Excel表中的顺序一致，不然会报错
+    public void login(String loginUrl,String partnerCode,String account, String loginPwd) throws Exception {
+        //使用构造函数将传入的用户名密码初始化成登录请求参数
+//        Manager loginParameters = new Manager(account,loginPwd,partnerCode);
+//        Map<String, String> map=FastjsonUtils.toMap(FastjsonUtils.toJson(loginParameters));
+//        //Log.info("my out**********"+map);
+//        //发送登录请求
+//        CloseableHttpResponse closeableHttpResponse = RestClient.postForm(hostManager+loginUrl, map, null);
+//        //从返回结果中获取状态码
+//        int statusCode = RestClient.getStatusCode(closeableHttpResponse);
+//        //Assert.assertEquals(statusCode,Constants.RESPNSE_STATUS_CODE_200,"status code is not 200");
+//        //断言响应json内容中name和job是不是期待结果
+//        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity());
+//        System.out.println("my out**********"+responseString);
+//        //JSONObject res = FastjsonUtils.toJsonObject(responseString);
+//        //sessionKey = FastjsonUtils.toMap(res.getString("data")).get("session_key");
+//        System.out.println("data**********: " + res.getString("message"));
+//        System.out.println("data**********: " + sessionKey);
+//        String partnerType=FastjsonUtils.toMap(res.getString("data")).get("partnerType");
+//        Assert.assertEquals(partnerType,"路内" ,"partnerType is not 路内");
+    }
+    @BeforeClass
+    public void endTest(){
+        System.out.print("测试结束");
+    }
 
 }
